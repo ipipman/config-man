86e7446cfa3ea7003911791de85e74213d02047b
ipipman <ipipman@163.com>
2024-05-26 10:17:18 +0800

    add filed util code commit

--- config-client/src/main/java/cn/ipman/config/client/utils/FieldUtils.java
+++ config-client/src/main/java/cn/ipman/config/client/utils/FieldUtils.java
@@ -6,12 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * 提供用于检索类中具有特定注解或满足某些条件的字段的工具方法
+ */
 public interface FieldUtils {
 
+    /**
+     * 查找类中所有被指定注解标注的字段
+     *
+     * @param aClass     要搜索的类。
+     * @param annotationClass 指定的注解类型。
+     * @return 所有被指定注解标注的字段列表。
+     */
     static List<Field> findAnnotatedField(Class<?> aClass, Class<? extends Annotation> annotationClass) {
         return findField(aClass, f -> f.isAnnotationPresent(annotationClass));
     }
 
+    /**
+     * 根据给定的函数条件查找类中所有满足条件的字段
+     *
+     * @param aClass          要搜索的类。
+     * @param function        用于判断字段是否满足条件的函数。
+     * @return 所有满足条件的字段列表。
+     */
     static List<Field> findField(Class<?> aClass, Function<Field, Boolean> function) {
         List<Field> result = new ArrayList<>();
         while (aClass != null) {
